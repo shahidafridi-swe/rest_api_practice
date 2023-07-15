9244cdc6873944c2d094538ef836f53687ebc6b5
shahidafridi-swe <shahidafridi.cse@gmail.com>
2023-07-16 00:46:59 +0600

    Simple JWT Based Login API

--- rest_api_practice/urls.py
+++ rest_api_practice/urls.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
-from django.conf.urls.static  import static
+from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login-api/', obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('rest_framework.urls')),
     path('api/firstapp/', include('firstapp.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
