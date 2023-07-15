0fee2e5ad168d6e794728eda3be97b6aff421428
shahidafridi-swe <shahidafridi.cse@gmail.com>
2023-07-15 22:49:01 +0600

    user registration API

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('first/', firstAPI)
+    path('first/', firstAPI),
+    path('registration/', registrationAPI),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -3,6 +3,41 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from django.contrib.auth.models import User
+
+
+@api_view(['POST'])
+def registrationAPI(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        email = request.data['email']
+        first_name = request.data['first_name']
+        last_name = request.data['last_name']
+        password1 = request.data['password1']
+        password2 = request.data['password2']
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "An user with this username already exists!"},headers={'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'})
+   
+    if User.objects.filter(email=email).exists():
+        return Response({"error": "An user with this email already exists!"},headers={'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'})
+   
+    if password1 != password2:
+        return Response({"error": "Two Password didn't match"},headers={'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'})
+    
+    user = User()
+    user.username=username
+    user.email=email
+    user.first_name=first_name
+    user.last_name=last_name
+    user.is_active=True
+    user.set_password(raw_password=password1)
+    user.save()
+
+    return Response({'success': 'User successfully resisterd!'},headers={'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'})
+
+
+
 @api_view(['GET', 'POST'])
 def firstAPI(request):
     if request.method=='POST':
@@ -15,3 +50,5 @@ def firstAPI(request):
         'country': 'Bangladesh'
     }
     return Response(context,headers={'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'})
+
+
