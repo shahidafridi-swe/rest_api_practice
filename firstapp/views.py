d1528496dcc759902a827842d53b406474b6300f
shahidafridi-swe <shahidafridi.cse@gmail.com>
2023-07-15 20:33:27 +0600

    create first django rest api using a function

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET', 'POST'])
+def firstAPI(request):
+    if request.method=='POST':
+        name = request.data['name']
+        age = request.data['age']
+        print(name, age)
+        return Response({'name': name, 'age':age})
+    context = {
+        'name': 'Shahid Afridi',
+        'country': 'Bangladesh'
+    }
+    return Response(context)
